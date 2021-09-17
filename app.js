51cdddd1dbc5ad71231a4cff83de7171b860722b
Muhammad yulianto <58633772+Emwaii@users.noreply.github.com>
2021-09-17 14:31:45 +0700

    first

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const countDate = new Date('May 25, 2021 00:00:00').getTime();
+  const countDate = new Date('September 30, 2021 00:00:00').getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
